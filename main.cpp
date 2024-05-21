c9c88be54c394cd30e05dd9482d1660f58fa7f4b
zimka1 <zimkail2015@mail.ru>
2024-05-22 00:03:16 +0200

    Added the ability to choose a user to send a message
    - Added a list of registered users on the client side.
    - The client can choose a registered user to send a private message.
    - Updated the interface for selecting a user and sending messages.
    - Added a check to ensure incoming data is text.
    
    Components:
    - Server: updated handlers for registration and message sending.
    - Client: added user list and functionality to select message recipients.

--- main.cpp
+++ main.cpp
@@ -1,61 +1,67 @@
 #include <iostream>
 #include <uwebsockets/App.h>
 #include <nlohmann/json.hpp>
+#include <unordered_map>
 using namespace std;
 using json = nlohmann::json;
 
-// 3. Users will have names
-// 4. How to know who is connected to the server?
-
+// Define user data structure
 struct UserData {
     int id;
     string name;
     string hisChat;
 };
 
+// Global container to store users
+unordered_map<int, UserData*> connectedUsers;
 
-// [USER 17] =server> {"command": "public_msg", "text": "a"}
-// [SERVER] =everyone> {"command": "public_msg", "text":"a", "user_from": 17}
-void process_public_msg(json parsed_data, uWS::WebSocket<false, true, UserData> *ws){
+// Process public message
+void process_public_msg(json parsed_data, uWS::WebSocket<false, true, UserData> *ws) {
     auto* udata = ws->getUserData();
-    string message = parsed_data["text"].dump();
-//    json payload = {
-//            {"command", "public_msg"},
-//            {"text", parsed_data["text"]},
-//            {"user_from", udata->id}
-//    };
-//    ws->publish("public", payload.dump());
+    string message = parsed_data["text"];
     ws->publish("public", udata->name + ": " + message);
 }
 
-// [USER 17] =server> {"command": "private_msg", "text": "hi", "user_to": 23}
-// [SERVER] =user23> {"command": "private_msg", "text": "hi", "user_from": 17}
+// Process private message
 void process_private_msg(json parsed_data, uWS::WebSocket<false, true, UserData> *ws) {
     auto* udata = ws->getUserData();
     int user_to = parsed_data["user_to"];
-    string message = parsed_data["text"].dump();
-//    json payload = {
-//            {"command", "private_msg"},
-//            {"text", parsed_data["text"]},
-//            {"user_from", udata->id}
-//    };
-    cout << "User " << udata->id << " send message to " << user_to << endl;
-//    ws->publish("user" + to_string(user_to), payload.dump());
-    ws->publish("user" + to_string(user_to), udata->name + ": " + message);
+    string message = parsed_data["text"];
 
+    cout << "User " << udata->id << " send message to " << user_to << endl;
+    ws->publish("user" + to_string(user_to), udata->name + ": " + message);
 }
 
-void process_registration(json parsed_data, uWS::WebSocket<false, true, UserData> *ws){
+// Process registration
+void process_registration(json parsed_data, uWS::WebSocket<false, true, UserData> *ws) {
     auto* udata = ws->getUserData();
     udata->name = parsed_data["name"];
 
+    // Send user ID back to client
+    json response = {
+            {"command", "registered"},
+            {"user_id", udata->id}
+    };
+    ws->send(response.dump(), uWS::OpCode::TEXT);
+
+    // Broadcast new user registration
     ws->publish("public", "New user " + to_string(udata->id) + " registered with name " + udata->name);
+
+    // Add user to global container
+    connectedUsers[udata->id] = udata;
+}
+
+// Process user list request
+void process_user_list(uWS::WebSocket<false, true, UserData> *ws) {
+    json userList = json::array();
+    for (const auto& userPair : connectedUsers) {
+        userList.push_back({{"id", userPair.second->id}, {"name", userPair.second->name}});
+    }
+    ws->send(userList.dump(), uWS::OpCode::TEXT);
 }
 
 int main() {
-    int latest_user_id = 10; // incremented each time
-
-    vector <UserData> Users;
+    int latest_user_id = 10; // Incremented each time
 
     uWS::App().ws<UserData>("/*", {
             .idleTimeout = 16,
@@ -71,12 +77,6 @@ int main() {
                 cout << "New user connected: " << data->id << endl;
                 ws->send("Welcome to BreezeTalk", uWS::OpCode::TEXT);
 
-                json welcome_message = {
-                        {"command", "welcome"},
-                        {"user_id", data->id}
-                };
-                ws->send(welcome_message.dump(), uWS::OpCode::TEXT);
-
                 ws->subscribe("public");
                 ws->subscribe(data->hisChat);
             },
@@ -85,17 +85,19 @@ int main() {
                 json parsed_data = json::parse(message);
                 if (parsed_data["command"] == "public_msg") {
                     process_public_msg(parsed_data, ws);
-                }
-                if (parsed_data["command"] == "private_msg"){
+                } else if (parsed_data["command"] == "private_msg") {
                     process_private_msg(parsed_data, ws);
-                }
-                if (parsed_data["command"] == "register"){
+                } else if (parsed_data["command"] == "register") {
                     process_registration(parsed_data, ws);
+                } else if (parsed_data["command"] == "user_list") {
+                    process_user_list(ws);
                 }
             },
             // someone disconnected from the server
-            .close = [](auto */*ws*/, int /*code*/, std::string_view /*message*/) {
-                /* You may access ws->getUserData() here */
+            .close = [](auto *ws, int /*code*/, std::string_view /*message*/) {
+                auto* data = ws->getUserData();
+                cout << "User disconnected: " << data->id << endl;
+                connectedUsers.erase(data->id); // Remove user from global container
             }
     }).listen(9001, [](auto *listen_socket) {
         if (listen_socket) {
